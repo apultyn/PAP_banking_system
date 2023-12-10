60cd0218010e7b81932c26a8a295d00746c38776
dmachnia <01178558@pw.edu.pl>
2023-12-10 14:38:43 +0100

    Add database connection

--- src/main/java/banking_app/classes/Main.java
+++ src/main/java/banking_app/classes/Main.java
@@ -1,7 +1,26 @@
 package banking_app.classes;
 
+import connections.OracleConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws SQLException {
+        Connection connection = OracleConnectionManager.getConnection();
+        String sqlQuery = "SELECT * FROM users";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    int userId = resultSet.getInt("user_id");
+                    String name = resultSet.getString("name");
+
+                    System.out.println("User ID: " + userId + ", Name: " + name);
+                }
+            }
+        }
     }
 }
