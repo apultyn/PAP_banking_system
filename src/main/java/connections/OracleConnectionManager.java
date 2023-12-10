9fbd0cd88aa6d4ec58f01827a53e2f71f25ef634
dmachnia <01178558@pw.edu.pl>
2023-12-10 15:10:47 +0100

    Change location of keys

--- src/main/java/connections/OracleConnectionManager.java
+++ src/main/java/connections/OracleConnectionManager.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class OracleConnectionManager {
 
-    private static final String TRUST_STORE_PATH = "/truststore.jks";
-    private static final String KEY_STORE_PATH = "/keystore.jks";
+    private static final String TRUST_STORE_PATH = "/Wallet_BankingSystem/truststore.jks";
+    private static final String KEY_STORE_PATH = "/Wallet_BankingSystem/keystore.jks";
     private static final String KEY_STORE_PASSWORD = "DruzynaDamyRade1";
     private static final String JDBC_URL = "jdbc:oracle:thin:@(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.eu-frankfurt-1.oraclecloud.com))(connect_data=(service_name=g35f0685faf01cb_bankingsystem_medium.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))";
     private static final String USERNAME = "admin";
