b86bb7d6042bf57d276ba382d9988f1c8b0455c6
krisg <krzysgolcz@gmail.com>
2023-12-10 19:59:09 +0100

    Connector

--- src/main/java/banking_app/classes/Main.java
+++ src/main/java/banking_app/classes/Main.java
@@ -1,26 +1,13 @@
 package banking_app.classes;
 
-import connections.OracleConnectionManager;
+import connections.ConnectionManager;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Connection connection = OracleConnectionManager.getConnection();
-        String sqlQuery = "SELECT * FROM users";
-
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                while (resultSet.next()) {
-                    int userId = resultSet.getInt("user_id");
-                    String name = resultSet.getString("name");
-
-                    System.out.println("User ID: " + userId + ", Name: " + name);
-                }
-            }
-        }
+        ConnectionManager manager = new ConnectionManager();
+        String outcome = manager.example();
+        System.out.println(outcome);
     }
 }
