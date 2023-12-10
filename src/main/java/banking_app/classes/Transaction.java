b900f79107069b5c6f63de390b70e0a3bfcd0ed8
krisg <krzysgolcz@gmail.com>
2023-12-10 22:08:34 +0100

    Get transaction from console

--- src/main/java/banking_app/classes/Transaction.java
+++ src/main/java/banking_app/classes/Transaction.java
@@ -4,6 +4,7 @@ import connections.ConnectionManager;
 
 import java.sql.Date;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Transaction
 {
@@ -17,6 +18,15 @@ public class Transaction
     public Transaction(int transactionId, int sourceId, int targetId,
                        Date date, float amount, int type, String title){
         this.transactionId = transactionId;
+        this.date = date;
+        this.sourceId = sourceId;
+        this.targetId = targetId;
+        this.amount = amount;
+        this.type = type;
+        this.title = title;
+    }
+    public Transaction(int sourceId, int targetId, Date date, float amount, int type, String title)
+    {
         this.date = date;
         this.sourceId = sourceId;
         this.targetId = targetId;
@@ -45,8 +55,22 @@ public class Transaction
     public int getType() {
         return type;
     }
+
+    public static Transaction getFromConsole(ConnectionManager connectionManager, Account sender) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj nr rachunku odbiorcy otzymujcego");
+        int reciever_acc_id = Integer.parseInt(sc.next());
+        System.out.println("Podaj kwote do przelania");
+        float amount = Float.parseFloat( sc.next());
+        System.out.println("Tytul");
+        String title = sc.next();
+        long mils = System.currentTimeMillis();
+        Date date = new Date(mils);
+        Transaction newTransaction = new Transaction(sender.getUserId(), reciever_acc_id, date, amount, 1, title);
+        registerTransaction(connectionManager, newTransaction);
+        return newTransaction;
+    }
     public static void registerTransaction(ConnectionManager connectionManager, Transaction newTransaction) throws SQLException {
         connectionManager.registerTransaction(newTransaction);
     }
-
 }
