c20d2f8791f57215b721126648418a67966b8fe4
Natalia <npieczko23@gmail.com>
2023-12-11 08:32:05 +0100

    Update Menu

--- src/main/java/banking_app/classes/Main.java
+++ src/main/java/banking_app/classes/Main.java
@@ -8,8 +8,10 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         ConnectionManager manager = new ConnectionManager();
 //        User.register(manager);
-        User user = User.login(manager);
-        System.out.println(user.getName());
+        Menu menu = new Menu(manager);
+        menu.menu();
+//        User user = User.login(manager);
+//        System.out.println(user.getName());
 //        String outcome = manager.example();
 //        Transaction newTransaction  = new Transaction(3, 4, 32.00f, "Pierwszy");
 //        Transaction.registerTransaction(manager, newTransaction);
