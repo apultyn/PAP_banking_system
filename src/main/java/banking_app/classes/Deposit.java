822ec57eefcee1f0cd0bcb06b34f79c211046825
Andrzej Pultyn <andrzej.pultyn.stud@pw.edu.pl>
2024-01-08 00:30:30 +0100

    Created DepositPanel, wrong end date from database

--- src/main/java/banking_app/classes/Deposit.java
+++ src/main/java/banking_app/classes/Deposit.java
@@ -9,7 +9,6 @@ import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class Deposit {
     private final int depositId;
@@ -17,7 +16,8 @@ public class Deposit {
     private BigDecimal amount;
     private BigDecimal rate;
     private long ownerAccId;
-    private Date start, end;
+    private Date start;
+    private Date end;
 
     public Deposit(int depositId, String name, BigDecimal amount,
                    BigDecimal rate, long ownerAccId, Date start, Date end) {
@@ -31,7 +31,6 @@ public class Deposit {
     }
 
     public Deposit(ResultSet resultSet) throws SQLException {
-
         this(resultSet.getInt("deposit_Id"),
                 resultSet.getString("name"),
                 resultSet.getBigDecimal("amount"),
@@ -65,7 +64,7 @@ public class Deposit {
     }
 
     public Date getEnd() {
-        return  end;
+        return end;
     }
     public void createDeposit(ConnectionManager manager) throws DepositNameExistingException, SQLException, NotEnoughFundsException, AccountNotFoundException {
         if (manager.findAccount(ownerAccId) == null)
@@ -88,21 +87,5 @@ public class Deposit {
                 ", start=" + start +
                 ", end=" + end +
                 '}';
-    }
-
-    public static void main(String[] args) throws SQLException {
-        ConnectionManager manager = new ConnectionManager();
-        Deposit deposit1 = new Deposit(0, "Pierwsze", new BigDecimal(0.1), new BigDecimal(5), 1000000000000046L,
-                new Date(2023, 1, 8), new Date(2023, 1, 9));
-        Deposit deposit2 = new Deposit(0, "Drugie", new BigDecimal(0.1), new BigDecimal(5), 1000000000000062L,
-                new Date(2023, 1, 8), new Date(2023, 1, 9));
-        Deposit deposit3 = new Deposit(0, "Trzecie", new BigDecimal(0.1), new BigDecimal(5), 1000000000000063L,
-                new Date(2023, 1, 8), new Date(2023, 1, 9));
-        manager.createDeposit(deposit2);
-        manager.createDeposit(deposit3);
-        ArrayList<Deposit> list = new ArrayList<>(manager.findUsersDeposits(85));
-        for (Deposit deposit : list) {
-            System.out.println(deposit);
-        }
     }
 }
--- src/main/java/banking_app/gui/DepositPanel.java
+++ src/main/java/banking_app/gui/DepositPanel.java
@@ -1,13 +1,116 @@
 package banking_app.gui;
 
+import banking_app.classes.Deposit;
 import banking_app.classes.User;
+import banking_exceptions.AccountNotFoundException;
+import banking_exceptions.DepositNameExistingException;
+import banking_exceptions.NotEnoughFundsException;
 import connections.ConnectionManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DepositPanel extends JPanel {
-    public DepositPanel(ConnectionManager manager, CardLayout cardLayout, JPanel cardPanel, User user) {
+    private JList<String> depositList;
+    private JLabel nameLabel;
+    private JLabel amountLabel;
+    private JLabel rateLabel;
+    private JLabel startDateLabel;
+    private JLabel endDateLabel;
+    private JLabel ownerAccIdLabel;
+    private JButton backButton;
+    private JButton createNewButton;
+    private ArrayList<Deposit> deposits;
 
+    public DepositPanel(ConnectionManager manager, CardLayout cardLayout, JPanel cardPanel, User user) throws SQLException {
+        setLayout(new BorderLayout());
+        deposits = new ArrayList<>(manager.findUsersDeposits(user.getId()));
+        for (Deposit deposit : deposits) {
+            System.out.println(deposit);
+        }
+
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        for (Deposit deposit : deposits) {
+            listModel.addElement(deposit.getName());
+        }
+        depositList = new JList<>(listModel);
+        depositList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        depositList.addListSelectionListener(e -> displaySelectedDeposit());
+
+        // Create labels for deposit information
+        nameLabel = new JLabel("Name:");
+        amountLabel = new JLabel("Amount:");
+        rateLabel = new JLabel("Rate:");
+        startDateLabel = new JLabel("Start Date:");
+        endDateLabel = new JLabel("End Date:");
+        ownerAccIdLabel = new JLabel("Owner Account Number:");
+
+        // Create buttons
+        backButton = new JButton("Go Back");
+        backButton.addActionListener(e -> goBack());
+        createNewButton = new JButton("Create New Deposit");
+        createNewButton.addActionListener(e -> createNewDeposit());
+
+        // Create a panel for deposit information
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new GridLayout(6, 2));
+        infoPanel.add(nameLabel);
+        infoPanel.add(new JLabel());
+        infoPanel.add(amountLabel);
+        infoPanel.add(new JLabel());
+        infoPanel.add(rateLabel);
+        infoPanel.add(new JLabel());
+        infoPanel.add(startDateLabel);
+        infoPanel.add(new JLabel());
+        infoPanel.add(endDateLabel);
+        infoPanel.add(new JLabel());
+        infoPanel.add(ownerAccIdLabel);
+        infoPanel.add(new JLabel());
+
+        // Add components to the main panel
+        add(new JScrollPane(depositList), BorderLayout.WEST);
+        add(infoPanel, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(backButton);
+        buttonPanel.add(createNewButton);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private void displaySelectedDeposit() {
+        int selectedIndex = depositList.getSelectedIndex();
+        if (selectedIndex >= 0) {
+            Deposit deposit = deposits.get(selectedIndex);
+            nameLabel.setText("Name: " + deposit.getName());
+            amountLabel.setText("Amount: " + deposit.getAmount());
+            rateLabel.setText("Rate: " + deposit.getRate());
+            startDateLabel.setText("Start Date: " + deposit.getStart());
+            endDateLabel.setText("End Date: " + deposit.getEnd());
+            ownerAccIdLabel.setText("Owner Account Number: " + deposit.getOwnerAccId());
+        }
+    }
+
+    private void goBack() {
+        // Implement go back functionality here
+    }
+
+    private void createNewDeposit() {
+        // Implement create new deposit functionality here
+    }
+
+    public static void main(String[] args) throws SQLException, NotEnoughFundsException, DepositNameExistingException, AccountNotFoundException {
+        ConnectionManager manager = new ConnectionManager();
+        User user = manager.findUser("przyklad@pw.edu.pl");
+//        Deposit deposit = new Deposit(0, "czwarte", new BigDecimal(19.94), new BigDecimal(2),
+//                1000000000000047L, new Date(2024, 1, 10), new Date(2024, 10, 11));
+//        deposit.createDeposit(manager);
+        DepositPanel depositPanel = new DepositPanel(manager, null, null, user);
+
+        JFrame frame = new JFrame("Bank Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.add(depositPanel);
+        frame.setVisible(true);
     }
 }
