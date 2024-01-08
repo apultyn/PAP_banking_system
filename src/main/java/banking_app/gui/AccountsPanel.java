420e8c9f59697760716ec65b3e79d0decbea8040
Natalia <npieczko23@gmail.com>
2024-01-08 22:45:54 +0100

    Redesign User Profile and start Transactions History

--- src/main/java/banking_app/gui/AccountsPanel.java
+++ src/main/java/banking_app/gui/AccountsPanel.java
@@ -1,15 +1,23 @@
 package banking_app.gui;
 
+import banking_app.classes.Account;
 import banking_app.classes.User;
 import connections.ConnectionManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLException;
+import java.util.List;
+
 
 public class AccountsPanel extends JPanel {
     private ConnectionManager manager;
     private CardLayout cardLayout;
     private JPanel cardPanel;
+
+    private JList<String> accountsList;
+    private DefaultListModel<String> accountsListModel; // Model dla JList
+
 
     private User user;
     public AccountsPanel(ConnectionManager manager, CardLayout cardLayout, JPanel cardPanel, String panelName){
@@ -19,9 +27,34 @@ public class AccountsPanel extends JPanel {
         this.cardLayout = cardLayout;
 
 
+        JButton returnButton = new JButton("Back");
+        accountsListModel = new DefaultListModel<>();
+        accountsList = new JList<>(accountsListModel);
+        JScrollPane scrollPane = new JScrollPane(accountsList);
+        accountsList.setFixedCellHeight(30); // Wysokość każdego elementu listy
+        //accountsList.setFixedCellWidth(150); // Szer
+
+        add(scrollPane); // Dodaj JScrollPane z JList do panelu
+        add(returnButton);
+
+        returnButton.addActionListener(e -> cardLayout.show(cardPanel, "User"));
     }
 
-    public void setUser(User user){
+    public void showAccounts() {
+        accountsListModel.clear();
+        try {
+            List<Account> accounts = manager.findUsersAccounts(user.getId());
+            for (Account account : accounts) {
+                String account_info = "Name: " + account.getName() + "Balance: " + account.getBalance();
+                accountsListModel.addElement(account_info);
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(this, "Accounts not found");
+        }
+    }
+
+    public void setUser(User user) {
         this.user = user;
+        showAccounts();
     }
 }
--- src/main/java/banking_app/gui/UserProfilePanel.java
+++ src/main/java/banking_app/gui/UserProfilePanel.java
@@ -1,21 +1,31 @@
 package banking_app.gui;
 
+import banking_app.classes.Account;
+import banking_app.classes.Transaction;
 import banking_app.classes.User;
 import connections.ConnectionManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserProfilePanel extends JPanel {
     private User user;
-
     private JLabel helloLabel;
-
     private ConnectionManager manager;
     private CardLayout cardLayout;
     private JPanel cardPanel;
+    private JComboBox<String> accountComboBox;
+    private Account currentAccount;
+    private JLabel accountDetailsLabel;
+    private JLabel balanceLabel;
+    private JPanel menuPanel;
+    private JPanel contentPanel;
 
-
+    private JTable transactionHistoryTable;
+    private TransactionsHistoryModel transactionHistoryModel;
 
     public UserProfilePanel(ConnectionManager manager, CardLayout cardLayout, JPanel cardPanel, String panelName) {
         this.setName(panelName);
@@ -24,7 +34,14 @@ public class UserProfilePanel extends JPanel {
         this.cardPanel = cardPanel;
         this.cardLayout = cardLayout;
 
-        helloLabel = new JLabel();
+        transactionHistoryModel = new TransactionsHistoryModel(new ArrayList<>());
+        transactionHistoryTable = new JTable(transactionHistoryModel);
+        JScrollPane scrollPane = new JScrollPane(transactionHistoryTable);
+
+        menuPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        contentPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         JButton transactionsButton = new JButton("Transactions");
         JButton contactsButton = new JButton("Contacts");
@@ -34,25 +51,91 @@ public class UserProfilePanel extends JPanel {
         JButton accountsButton = new JButton("My Accounts");
         JButton standingOrdersButton = new JButton("Standing Orders");
 
-        add(helloLabel);
-        add(transactionsButton);
-        add(contactsButton);
-        add(transactionHistoryButton);
-        add(modifyProfileButton);
-        add(automaticSavingButton);
-        add(accountsButton);
-        add(standingOrdersButton);
+        menuPanel.add(transactionsButton);
+        menuPanel.add(contactsButton);
+        menuPanel.add(transactionHistoryButton);
+        menuPanel.add(modifyProfileButton);
+        menuPanel.add(automaticSavingButton);
+        menuPanel.add(accountsButton);
+        menuPanel.add(standingOrdersButton);
 
         modifyProfileButton.addActionListener(e -> handleModifyButton());
         automaticSavingButton.addActionListener(e -> handleAutomaticSavings());
         accountsButton.addActionListener(e -> handleAccountsButton());
         standingOrdersButton.addActionListener(e->handleStandingOrders());
 
+        setLayout(new BorderLayout());
+        add(menuPanel, BorderLayout.NORTH); // Add the menu panel at the top
+
+        helloLabel = new JLabel();
+        accountComboBox = new JComboBox<>();
+        accountDetailsLabel = new JLabel();
+        balanceLabel = new JLabel();
+
+        helloLabel.setFont(helloLabel.getFont().deriveFont(20f));
+        balanceLabel.setFont(balanceLabel.getFont().deriveFont(16f));
+
+        accountComboBox.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        accountComboBox.setMaximumSize(new Dimension(Integer.MAX_VALUE, 60));
+
+        contentPanel.add(helloLabel);
+        contentPanel.add(Box.createVerticalStrut(10));
+        contentPanel.add(accountComboBox);
+        contentPanel.add(Box.createVerticalStrut(10));
+        contentPanel.add(accountDetailsLabel);
+        contentPanel.add(Box.createVerticalStrut(10));
+        contentPanel.add(balanceLabel);
+
+        accountComboBox.addActionListener(e -> onAccountSelected());
+
+        setLayout(new BorderLayout());
+        add(menuPanel, BorderLayout.NORTH);
+        add(contentPanel, BorderLayout.CENTER);
+        add(scrollPane, BorderLayout.SOUTH);
+
     }
 
     public void setUser(User setted_user) {
         user = setted_user;
         helloLabel.setText("Hello " + user.getName());
+        try {
+            List<Account> accounts = manager.findUsersAccounts(user.getId());
+            setAccounts(accounts);
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(this, e);
+        }
+
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        accountComboBox.removeAllItems();
+        for (Account account : accounts) {
+            String item = account.getName() + " - " + account.getAccountId();
+            accountComboBox.addItem(item);
+        }
+    }
+
+    private void onAccountSelected() {
+        String selected = (String) accountComboBox.getSelectedItem();
+        if (selected != null) {
+            String[] parts = selected.split(" - ");
+            String accountName = parts[0];
+            String accountId = parts.length > 1 ? parts[1] : "";
+
+            try {
+                currentAccount = manager.findAccount(Long.parseLong(accountId));
+                String account_info = "Choosen account: " + currentAccount.getName() + ", Account ID: " + currentAccount.getAccountId();
+                accountDetailsLabel.setText(account_info);
+                String balance_info = String.format("Balance: %.2f zł", currentAccount.getBalance());
+                balanceLabel.setText(balance_info);
+
+                List<Transaction> history = manager.findTransactionsBySender(currentAccount.getAccountId());
+                transactionHistoryModel = new TransactionsHistoryModel(history);
+                transactionHistoryTable.setModel(transactionHistoryModel);
+            } catch (SQLException e){
+                JOptionPane.showMessageDialog(this, e);
+            }
+        }
     }
 
     public void  handleModifyButton() {
